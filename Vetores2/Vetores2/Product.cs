30e0cb6f9b69e2f08e7c70c213ccaca6be8d06a6
brenogaudencio <brenobichels123@gmail.com>
2026-03-02 22:35:41 -0300

    Vetores2

---
+++ Vetores2/Vetores2/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Vetores2
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
