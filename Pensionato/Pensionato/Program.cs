03a193833ce389ac0f7ec7945e35a46e3b6111e2
brenogaudencio <brenobichels123@gmail.com>
2026-03-02 23:30:20 -0300

    Vetores com classe

---
+++ Pensionato/Pensionato/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace BoardingHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rents[] vect = new Rents[10];
+           
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Rent #" + (j + 1) + ":");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int rent = int.Parse(Console.ReadLine());
+                vect[rent] = new Rents (name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Pensionato/Pensionato/Rent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardingHouse
+{
+    internal class Rents
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Rents(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Email;
+        }
+    }
+}
