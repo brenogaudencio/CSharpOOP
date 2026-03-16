ee7712ad837a90492108db90b94b31284fe3d84a
brenogaudencio <brenobichels123@gmail.com>
2026-03-16 19:11:45 -0300

    foreach

---
+++ 01/01/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace _01
+{
+
+    internal class Calculator
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ 01/01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using _01;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(1, 2, 3);
+            int s2 = Calculator.Sum(4, 5);
+            
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+
+    }
+} 
